9d1d86dea639a871ca88f946ecdfb00aea840c5d
LoraBel07 <91973134+LoraBel07@users.noreply.github.com>
2022-08-31 14:52:02 -0400

    commit

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,5 @@
 import { Button, Card } from "react-bootstrap"
+import { useShoppingCart } from "../context/ShoppingCartContext"
 import { formatCurrency } from "../utilities/formatCurrency"
 
 type StoreItemProps = {
@@ -9,7 +10,8 @@ type StoreItemProps = {
 }
 
 export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
-	const quantity = 0
+	const { getItemQuantity, increaseCartQuantity, decreaseCartQuantity, removeFromCart } = useShoppingCart()
+	const quantity = getItemQuantity(id)
 	return(
 		<Card className="h-100">
 			<Card.Img variant="top" src={imgUrl} height="200px" style={{ objectFit: "cover"}}/>
@@ -20,16 +22,16 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
 				</Card.Title>
 				<div className="mt-auto">
 					{quantity === 0 ? (
-						<Button className="w-100"> ➕ Add to Cart </Button>
+						<Button className="w-100" onClick={() => increaseCartQuantity(id)}> ➕ Add to Cart </Button>
 					) : <div className="d-flex align-items-center flex-column" style={{ gap: ".5rem" }}>
 						<div className="d-flex align-items-center justify-content-center" style={{ gap: ".5rem" }}>
-							<Button> ➖ </Button>
+							<Button onClick={() => decreaseCartQuantity(id)}> ➖ </Button>
 							<div>
 							<span className="fs-3">{quantity}</span> in Cart
 							</div>
-							<Button> ➕ </Button>
+							<Button onClick={() => increaseCartQuantity(id)}> ➕ </Button>
 						</div>
-						<Button variant="danger" size="sm">Remove</Button>
+						<Button onClick={() => removeFromCart(id)} variant="danger" size="sm">Remove</Button>
 						</div>}
 				</div>
 			</Card.Body>
