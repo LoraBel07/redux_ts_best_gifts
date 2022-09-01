52a97295b0b01447849b79cf8b1c5e378ee4ecc7
LoraBel07 <91973134+LoraBel07@users.noreply.github.com>
2022-09-01 00:27:27 -0400

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,8 @@ function App() {
       <Navbar />
       <Container className="mb-4">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/store" element={<Store />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Store />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </Container>
