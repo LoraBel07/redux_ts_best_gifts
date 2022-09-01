192e2c11537e663da3237f54a6967dba1dfc77a8
LoraBel07 <91973134+LoraBel07@users.noreply.github.com>
2022-09-01 00:19:51 -0400

    commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ export function Navbar() {
 	const { openCart, cartQuantity } = useShoppingCart()
 
 	return (
-	<NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
+	<NavbarBs sticky="top" className="bg-light mb-3">
 		<Container>
 			<Nav className="me-auto">
 				<Nav.Link to="/" as={NavLink}> 💝 </Nav.Link>
